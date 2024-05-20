d0758a948dbf17e7fa0f96a4834d44ed2c30e1bb
867380899 <867380899@qq.com>
2024-05-20 21:39:17 +0800

    feat: basic markdown config

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"MeanderingProgrammer/markdown.nvim",
+		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+		dependencies = { "nvim-treesitter/nvim-treesitter" }, -- need to install treesitter-latex
+		config = function()
+			require("render-markdown").setup({
+        file_types = { 'markdown', 'norg' },
+				headings = { " ", " ", " ", " ", " ", " " },
+				highlights = {
+					heading = {
+						backgrounds = {},
+					},
+				},
+			})
+		end,
+	}
+}
