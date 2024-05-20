e1a2517ccb272b7d378d5a39ec005f633e14fe59
867380899 <867380899@qq.com>
2024-05-20 20:23:03 +0800

    feat: autotag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,31 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"vimdoc",
-				"bash",
-				"javascript",
-				"python",
-				"markdown",
-				"markdown_inline",
-				"css",
-				"html",
-				"json",
-				"yaml",
-			},
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  {
+    "windwp/nvim-ts-autotag",
+    config = function ()
+      require('nvim-ts-autotag').setup()
+    end
+  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"vimdoc",
+					"bash",
+					"javascript",
+					"python",
+					"markdown",
+					"markdown_inline",
+					"css",
+					"html",
+					"json",
+					"yaml",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
